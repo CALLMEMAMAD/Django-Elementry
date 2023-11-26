1ab86064eb4f00a8826295c1fd212f9f811daea8
mohammad_harmi <95447590+CALLMEMAMAD@users.noreply.github.com>
2023-11-26 21:58:53 +0330

    Update tests.py

--- pages/pages/tests.py
+++ pages/pages/tests.py
@@ -4,10 +4,16 @@ class HomepageTests(SimpleTestCase):
    def test_url_exists_at_correct_location(self):
        response = self.client.get("/")
        self.assertEqual(response.status_code, 200)
+   def test_url_available_by_name(self):
+       response = self.client.get(reverse("home"))
+       self.assertEqual(response.status_code, 200)
 
 class AboutpageTests(SimpleTestCase):
    def test_url_exists_at_correct_location(self):
        response = self.client.get("/about/")
+       self.assertEqual(response.status_code, 200)
+   def test_url_available_by_name(self):
+       response = self.client.get(reverse("about"))
        self.assertEqual(response.status_code, 200)
   
 """for run this one please type in terminal: python manage.py test """
